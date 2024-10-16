d3a79bc89e1bc99ca9719d52a94ab9c7599c9478
Chahat Shah <chahatshah2002@gmail.com>
2024-10-16 20:20:31 +0530

    Second Commit

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { updateUser } from "../controllers/user.controller.js";
+import { deleteUser, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const userRouter = express.Router();
 userRouter.post("/update/:id", verifyToken, updateUser);
+userRouter.delete("/delete/:id", verifyToken, deleteUser);
 export default userRouter;
